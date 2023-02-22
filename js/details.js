83744e4e2c55ecc9013157f5da04c9d61136c14d
LineNilsen <Lineforslund@hotmail.com>
2023-02-22 10:13:16 +0100

    js

--- js/details.js
+++ js/details.js
@@ -18,15 +18,35 @@ async function fetchDrink(){
     const json = await response.json();
   
     console.log(json)
-    container.innerHTML = `<h1>${json.drinks[0].strDrink}</h1>
-                           <b>Recommended glass:</b>
-                           <p>${json.drinks[0].strGlass}</p>  
-                           <b>Ingredients:</b>
-                           <p>Subscribe for ingredients</p>
-                           <b>Instructions:</b>
-                           <p>${json.drinks[0].strInstructions}</p>
-                           <b>Instructions (Italian):</b>
-                           <p>${json.drinks[0].strInstructionsIT}</p>`;
+
+    json.forEach((details) => {
+
+      const newDiv = document.createElement("div");
+
+      const newH1 = document.createElement("h1");
+      newH1.textContent = `${details.strDrink}`;
+
+      const newP = document.createElement("p");
+      newP.textContent = `Recommended glass:`;
+
+
+      newDiv.appendChild(newH1);
+      newDiv.appendChild(newP);
+
+      container.appendChild(newDiv);
+
+
+
+    });
+    //container.innerHTML = `<h1>${json.drinks[0].strDrink}</h1>
+    //                       <b>Recommended glass:</b>
+    //                       <p>${json.drinks[0].strGlass}</p>  
+    //                       <b>Ingredients:</b>
+    //                       <p>Subscribe for ingredients</p>
+    //                       <b>Instructions:</b>
+    //                       <p>${json.drinks[0].strInstructions}</p>
+    //                       <b>Instructions (Italian):</b>
+    //                       <p>${json.drinks[0].strInstructionsIT}</p>`;
   
   }
   catch(error) {
